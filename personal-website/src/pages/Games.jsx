07dc4b04e3e6a5c6c012ffaf3e4157f4ec702f1d
aadya <110504381+adex1a@users.noreply.github.com>
2026-03-08 19:12:25 -0700

    initial filler for games  page

--- personal-website/src/pages/Games.jsx
+++ personal-website/src/pages/Games.jsx
@@ -1,11 +1,82 @@
 import React from "react";
 import "./Home.css";
+import "../App.css";
+import "../index.css";
 
 const Games = () => {
   return (
     <div>
-      <h1 className="Home-text">Games page</h1>
-      <p> specific section on my games, link to itch page</p>
+      {/* <h1 className="Home-text">Games page</h1> */}
+      <p className="Pages-paragraphText">
+        I've been making games independently for nearly four years now, starting
+        in 2022 with the COSMOS Game Design summer program and continuing
+        through 2024, when I was chosen to be an Entertainment Software
+        Association Scholar, to 2026, when I am attending the GDC Festival of
+        Gaming. I've covered many roles in gamedev, from programming to music,
+        though these days, I'm most interested in writing and narrative design.
+        Below are some games I've been involved in relatively recently.
+      </p>
+      <h2>Highlighted Games</h2>
+      <h4>
+        <a href="https://adexia.itch.io/cirque-des-animaux" target="_blank">
+          Cirque des Animaux
+        </a>
+      </h4>
+      <section className="music">
+        <img
+          src="src/assets/cda_sc.png"
+          alt="A screenshot of the main character, Kat, thinking about how handsome the villain is."
+        />
+        <aside className="Pages-paragraphText">
+          A dark fantasy RPG that placed #1 for Story and #4 Overall in the
+          Themed Horror Game Jam 21 on itch.
+        </aside>
+      </section>
+
+      <h4>
+        <a href="https://cinnamew.itch.io/dermimis" target="_blank">
+          Dermimi's
+        </a>
+      </h4>
+      <section className="music">
+        <img
+          src="src/assets/dermimis_sc.png"
+          alt="A screenshot of the main character, Mimi, and her shy girlfriend, Domo."
+        />
+        <p className="Pages-paragraphText">
+          A dreamy, sapphic visual novel that placed #7 Overall out of over 200
+          entries in the 2024 Winter MelonJam on itch.
+        </p>
+      </section>
+
+      <h4>
+        <a href="https://adexia.itch.io/the-love-application" target="_blank">
+          The Love Application
+        </a>
+      </h4>
+      <section className="music">
+        <img
+          src="src/assets/tla_sc.png"
+          alt="A screenshot of the main character, Stacy, with her best friend, Mia."
+        />
+        <p className="Pages-paragraphText">
+          A wholesome matchmaking-sim visual novel, made in 72 hours for the
+          2023 Winter MelonJam on itch.
+        </p>
+      </section>
+
+      {/*https://cinnamew.itch.io/dermimis
+      https://adexia.itch.io/cirque-des-animaux
+      https://adexia.itch.io/ost-cirque-des-animaux
+      https://adexia.itch.io/the-love-application*/}
+      <br />
+      <p className="Home-smallText">
+        See all my published games on my{" "}
+        <a href="https://adexia.itch.io/" target="_blank">
+          itch page
+        </a>
+        !
+      </p>
     </div>
   );
 };
--- personal-website/src/pages/Music.jsx
+++ personal-website/src/pages/Music.jsx
@@ -2,18 +2,35 @@ import React from "react";
 import "./Home.css";
 import "../App.css";
 import "../index.css";
+import { NavLink } from "react-router-dom";
 
 const Music = () => {
   return (
     <div>
       {/* <h1 className="Home-text">Music page</h1> */}
-      <h2>My Background</h2>
+      {/* <h2>My Background</h2> */}
       <p className="Pages-paragraphText">
-        My music journey began with violin lessons in 3rd grade, and piano soon
+        My music journey began with violin lessons in 3rd grade and piano soon
         after. While I quit piano four years later, I continued violin
-        seriously, joining several youth orchestras and attending Orange County
-        School of the Arts (OCSA) in the Instrumental Music: Strings & Orchestra
-        conservatory.
+        seriously, joining several youth orchestras including the Pacific
+        Symphony Youth Orchestra (PSYO), with whom I toured Vienna and Prague in
+        2023. I attended{" "}
+        <a href="https://www.ocsarts.net/" target="_blank">
+          Orange County School of the Arts (OCSA)
+        </a>{" "}
+        for high school, in the Instrumental Music: Strings & Orchestra
+        conservatory. At OCSA, beyond my classical performance, theory, and
+        composition classes, I also participated in the MONTAGE! pop performance
+        group and had the opportunity to play in gigs around the Orange County
+        area. These days, I still play violin in the{" "}
+        <a
+          href="https://youtube.com/@mitvgo?si=QYt448FaDn5cca8H"
+          target="_blank"
+        >
+          MIT Video Game Orchestra
+        </a>
+        , which I also serve on the executive board of; I also still love
+        composing music for my own games.
       </p>
 
       <h2>My Compositions</h2>
@@ -25,7 +42,10 @@ const Music = () => {
           <source src="src/assets/kiss of death.mp3" type="audio/mpeg" />
           Your browser does not support the audio element.
         </audio>
-        <aside>All I had was Musescore4 and a dream.</aside>
+        <aside>
+          All I had was Musescore4 and a dream. Made for one of my{" "}
+          <NavLink to="/games">games</NavLink>.
+        </aside>
       </section>
 
       <h4>Amused to Death</h4>
@@ -35,7 +55,9 @@ const Music = () => {
           <source src="src/assets/amused to death.mp3" type="audio/mpeg" />
           Your browser does not support the audio element.
         </audio>
-        <aside>Made for the same game, if "Death" didn't give it away.</aside>
+        <aside>
+          Made for the same game as above, if "Death" didn't give it away.
+        </aside>
       </section>
 
       <h4>Ersatz</h4>
@@ -51,7 +73,13 @@ const Music = () => {
           />
           Your browser does not support the audio element.
         </audio>
-        <aside>First time using the big boy DAW Reaper.</aside>
+        <aside>
+          First time using the big boy DAW Reaper. Made for{" "}
+          <a href="https://coco-adventurer.itch.io/ersatz" target="_blank">
+            this game
+          </a>
+          .
+        </aside>
       </section>
 
       <h4>Weird Cat Cafe</h4>
